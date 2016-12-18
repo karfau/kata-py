2702b136d4e6258d23e1dfef55c4c53a139f012c
Christian Bewernitz <coder@karfau.de>
2016-12-18 10:44:48 +0100

    main takes agruments from stdin or argv

--- src/script.py
+++ src/script.py
@@ -1,10 +1,14 @@
 #!python
 
 
-def main():
+def main(*args):
     return "returned from main"
 
 if __name__ == '__main__':
     import sys
     print "provided arguments:", sys.argv
-    print main()
+    if len(sys.argv) > 1:
+        print main(sys.argv[1:])
+    else:
+        for line in sys.stdin:
+            print main(line.split(', '))
